48fbd8f292b98e4756f4046bfdcf8e1fe185da7f
swapnilpatil-github <swapnilpatil1012003@gmail.com>
2024-08-18 13:37:12 +0530

    Vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Vite configuration
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'https://initial-web-projects.onrender.com'
